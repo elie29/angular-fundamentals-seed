69b300c527d236001719d8b63f3aa81c000f8f32
Elie NEHME <elie29@gmail.com>
2018-01-12 11:56:01 +0100

    Add ngForm

--- app/passengers/components/passenger-form/passenger-form.component.ts
+++ app/passengers/components/passenger-form/passenger-form.component.ts
@@ -6,9 +6,22 @@ import { Passenger } from '../../models/passenger.interface';
   selector: 'passenger-form',
   styleUrls: ['passenger-form.component.scss'],
   template: `
-    <form>
-    Form!
-    <div>{{ detail | json }}</div>
+    <form #form="ngForm">
+      {{ detail | json }}
+      <div>
+        Passenger Name: <input
+          type="text"
+          name="fullname"
+          [ngModel]="detail?.fullname"/>
+      </div>
+
+      <div>
+        Passenger ID: <input
+          type="text"
+          name="id"
+          [ngModel]="detail?.id"/>
+      </div>
+      {{ form.value | json }}
     </form>
   `
 })
